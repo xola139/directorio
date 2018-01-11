50d7b22aef6be55cf406744f3bd90533c90630f6
Ruflex <ulfixdev@localhost.localdomain>
2018-01-10 18:15:51 -0600

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { BookService } from './book.service';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { PromosComponent } from './promos/promos.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: BookComponent },
+  { path: 'book-details/:id', component: BookDetailComponent },
+  { path: 'book-create', component: BookCreateComponent },
+  { path: 'book-edit/:id', component: BookEditComponent },
+  
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent,
+    PromosComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    
+    FormsModule,
+    
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    BookService,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/book-edit/book-edit.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-book-edit',
+  templateUrl: './book-edit.component.html',
+  styleUrls: ['./book-edit.component.css']
+})
+export class BookEditComponent implements OnInit {
+
+  book = {};
+
+  constructor(private route: ActivatedRoute, private router: Router, private bookService: BookService) { }
+	
+  ngOnInit() {
+    this.getBook(this.route.snapshot.params['id']);
+  }
+
+  getBook(id) {
+    this.bookService.showBook(id).then((res) => {
+      this.book = res;
+      console.log(this.book);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  updateBook(id) {
+    this.bookService.updateBook(id, this.book).then((result) => {
+      let id = result['_id'];
+      this.router.navigate(['/book-details', id]);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  deleteBook(id) {
+  this.bookService.deleteBook(id).then((result) => {
+    this.router.navigate(['/books']);
+  }, (err) => {
+    console.log(err);
+  });
+}
+
+}
