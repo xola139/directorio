271652b880691d563ca0d78aabbc8852778ef58f
Your Name <xola139@gmail.com>
2018-01-11 23:22:46 -0600

    add for

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./model.component.css']
 })
 export class ModelComponent implements OnInit {
+	people: any[] = [
+    {
+      "id":1,
+      "name": "Douglas  Pace"
+    },
+    {
+    	"id":2,
+      "name": "Mcleod  Mueller"
+    },
+    {
+    	"id":3,
+      "name": "Day  Meyers"
+    },
+    {
+    	"id":4,
+      "name": "Aguirre  Ellis"
+    },
+    {
+    	"id":6,
+      "name": "Cook  Tyson"
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
