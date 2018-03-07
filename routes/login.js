b15720d17b3f1b637b4223f77ad6584c9ee84b7a
Your Name <xola139@gmail.com>
2018-03-07 07:40:15 -0600

    add view jade

---
+++ routes/login.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+
+// Ruta para autenticarse con Twitter (enlace de login)
+//router.get('/', passport.authenticate('twitter'));
+
+
+	// route for twitter authentication and login
+	// different scopes while logging in
+	router.get('/login/twitter', 
+		passport.authenticate('twitter'));
+
+
+
+
+
+
+// Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
+// En caso de fallo redirige a otra vista '/login'
+router.get('/auth/twitter/callback', passport.authenticate('twitter',
+  { successRedirect: '/', failureRedirect: '/model' }
+));
+
+
+
+module.exports = router;
