daa7a18929084f6e2ee457392fd7d6739d22b5b5
Raul Corona <xola139@gmail.com>
2018-04-24 10:13:43 -0500

    some changes config's

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,6 +1,6 @@
 // global.ts
 
  export const GlobalVariable = Object.freeze({
-     BASE_API_URL: 'http://104.236.211.184:3000'
+     BASE_API_URL: 'http://localhost:3000'
      //... more of your variables
  });
