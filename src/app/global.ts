e7b05148da535dfe9181cb0a911db22a32db433f
ruflex <xola139@gmail.com>
2018-04-24 04:51:51 -0500

    agregando mas detalle

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,6 +1,6 @@
 // global.ts
 
  export const GlobalVariable = Object.freeze({
-     BASE_API_URL: 'http://104.236.211.184:3000'
+     BASE_API_URL: 'http://45.55.201.161:3000'
      //... more of your variables
  });
