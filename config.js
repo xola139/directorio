7141cd9056922564501641e0a7a626431d0e037f
ruflex <xola139@gmail.com>
2018-04-26 00:21:01 -0500

    cambio config

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 var config={
 	  twitter:{
-                key:'',
-                secret:''
+                key:'sFkfRqeZJbpeDNRY5AEi26yM7',
+                secret:'zIYX0eY3GcfrwElGYYjuVm5LI421wG02C8WuMaLU107Owq6hay'
         },
         facebook:{
-                id:'',
-                secret:''
+                id:'asdadasdasd',
+                secret:'zIYX0eY3GcfrwElGYYjuVm5LI421wG02C8WuMaLU107Owq6hay'
         }
         ,conectDB:{
-                link:''
+                link:'mongodb://usuariogeneric:sharkesink139@ds041603.mlab.com:41603/generic'
 
         }
 	
 };
 
 module.exports= config;
+
+
