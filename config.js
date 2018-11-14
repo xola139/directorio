4b213a87bfff9ce5b121ddc052727849c5dad732
Raul Corona <xola139@gmail.com>
2018-11-13 18:12:38 -0600

    agregando variables de entorno

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
+var KEY = process.env.KEY || 'your_key';
+var SECRET = process.env.SECRET || 'your_secret';
+var TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN || 'twitter_access_token';
+var TWITTER_ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET || 'twitter_access_token_secret';
+var ULR_MONGO =  process.env.ULR_MONGO || 'twitter_access_token_secret';
+
+
 var config={
     twitter:{
-                key:'',
-                secret:'',
+                key: KEY,
+                secret:SECRET,
         
-               TWITTER_ACCESS_TOKEN: '',
-               TWITTER_ACCESS_TOKEN_SECRET: ''
+               TWITTER_ACCESS_TOKEN: TWITTER_ACCESS_TOKEN,
+               TWITTER_ACCESS_TOKEN_SECRET: TWITTER_ACCESS_TOKEN_SECRET
        }
         ,conectDB:{
-                link:''
+                link:ULR_MONGO
                 			
     }
   
@@ -15,3 +22,5 @@ var config={
 
 
 module.exports= config;
+
+
--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -13,152 +13,144 @@ import { LoaderService } from '../loader.service';
   styleUrls: ['./model.component.css','../app.component.css']
 })
 export class ModelComponent implements OnInit {
-  myData :any;
-  verItems :number;
-  disponibles :any;
-  promos :any;
-  pictures:any;
-  tipoFoto:any;
-  hideme=[];
-  isMobile :Boolean;
-  items:any; 
-  chooseCiudad:any;
-  ciudades=[];
-  lstCiudades:any;
-  Allciudades:any;
+    myData: any;
+    verItems: number;
+    disponibles: any;
+    promos: any;
+    pictures: any;
+    tipoFoto: any;
+    hideme = [];
+    isMobile: Boolean;
+    items: any;
+    chooseCiudad: any;
+    ciudades = [];
+    lstCiudades: any;
+    Allciudades: any;
 
-constructor( private loaderService: LoaderService, 
-             private modelService: ModelService,
-             private router: Router,
-             private route: ActivatedRoute ) {
-  this.disponibles = [];
-  this.promos = [];
-  this.verItems = 9;
-  this.pictures = [];
-  
- }
+    constructor(private loaderService: LoaderService,
+        private modelService: ModelService,
+        private router: Router,
+        private route: ActivatedRoute) {
+        this.disponibles = [];
+        this.promos = [];
+        this.verItems = 9;
+        this.pictures = [];
 
-
-
-
-
-
-  
-  ngOnInit() {
-
-    //http call starts
-    this.loaderService.display(true);
-
-    if(navigator.userAgent.indexOf("Mobile") > 0){
-         this.isMobile = true; 
-      }else{
-        this.isMobile= false;
     }
+
+    ngOnInit() {
+
+        //http call starts
+        this.loaderService.display(true);
+
+        if (navigator.userAgent.indexOf("Mobile") > 0) {
+            this.isMobile = true;
+        } else {
+            this.isMobile = false;
+        }
+
+        this.getModelos();
+        this.Allciudades = this.modelService.getAllCiudades();
+
+    }
+
+    mostratLoader() {
+        this.loaderService.display(true);
+        //$('#modalLoader').modal('show');
+    }
+
+    mostrarProfile() {
+      $('#exampleModalLong').on('shown.bs.modal',function(){      //correct here use 'shown.bs.modal' event which comes in bootstrap3
+        $(this).find('iframe').attr('src','/#/registro/DianaRolove')
+      })
+    }
+
     
-    this.getModelos();
-    this.Allciudades = this.modelService.getAllCiudades();
 
-  }
 
-mostratLoader(){
-  this.loaderService.display(true);
-  //$('#modalLoader').modal('show');
-}
-
-  verPorCiudad(){
-    var _item = [];
-    for(var x=0;x< this.items.length;x++){
-      if(this.chooseCiudad =='TODOS' || this.items[x].ciudad == this.chooseCiudad ){
-        _item.push(this.items[x]); 
-      }
+    verPorCiudad() {
+        var _item = [];
+        for (var x = 0; x < this.items.length; x++) {
+            if (this.chooseCiudad == 'TODOS' || this.items[x].ciudad == this.chooseCiudad) {
+                _item.push(this.items[x]);
+            }
+        }
+        this.myData = _item;
     }
-    this.myData = _item;
-  }
 
-
-
-  
-  getModelos() {
-    var _id = this.route.snapshot.params['id'];
-    this.modelService.showModelos(_id).then((res) => {
-    this.myData = this.items = res ; 
-    this.getLstCiudades();
-    this.loaderService.display(false);
-    }, (err) => {
-      console.log(err);
-    });
-  }
-
-  getLstCiudades() {
-    this.modelService.getCiudades().then((res) => {
-    this.lstCiudades = res ; 
-    }, (err) => {
-      console.log(err);
-    });
-  }
-
-
-
- verPromos(){
-  
-
-    var _item = [];
-    for(var x=0;x< this.items.length;x++){
-      console.log("tipo "+this.items[x].tipo);
-      if(this.items[x].tipo == 'promo' ){
-        _item.push(this.items[x]); 
-      }
-    }
-    this.myData = _item;
- }
-
-
-
-  getPictures(data,tipoFoto,idImg){
-    this.pictures = data.images;
-    this.pictures.id = data.id;
-
-    setTimeout(() => {
-      var top = $('#'+idImg).position().top;
-      $('#exampleModalLong').animate({scrollTop:(top - 50)}, 'slow');
-      },1000);
-
-  }
-
-
-
-  getVerMas(){
-
-    if(this.verItems < this.myData.length){
-      //this.verItems +=3;
-      for(var x=0;x< 3;x++){
-        this.verItems++;
-        this.modelService.getModelo(this.myData[this.verItems].id).then((res) => {
-          this.myData[this.verItems] = res
+    getModelos() {
+        var _id = this.route.snapshot.params['id'];
+        this.modelService.showModelos(_id).then((res) => {
+            this.myData = this.items = res;
+            this.getLstCiudades();
+            this.loaderService.display(false);
         }, (err) => {
-          console.log(err);
+            console.log(err);
         });
+    }
 
-      }
+    getLstCiudades() {
+        this.modelService.getCiudades().then((res) => {
+            this.lstCiudades = res;
+        }, (err) => {
+            console.log(err);
+        });
+    }
 
-      
 
+
+    verPromos() {
+        var _item = [];
+        for (var x = 0; x < this.items.length; x++) {
+            console.log("tipo " + this.items[x].tipo);
+            if (this.items[x].tipo == 'promo') {
+                _item.push(this.items[x]);
+            }
+        }
+        this.myData = _item;
+    }
+
+
+
+    getPictures(data, tipoFoto, idImg) {
+        this.pictures = data.images;
+        this.pictures.id = data.id;
+
+        setTimeout(() => {
+            var top = $('#' + idImg).position().top;
+            $('#exampleModalLong').animate({
+                scrollTop: (top - 50)
+            }, 'slow');
+        }, 1000);
 
     }
-  }
 
-  gotoCard(id){
-      var top = $('#card_' + id).position().top;
-      $('html, body').animate({scrollTop:(top - 60)}, 'slow');
-      
-      
-      
-  }
+    getVerMas() {
+        if (this.verItems < this.myData.length) {
+            //this.verItems +=3;
+            for (var x = 0; x < 3; x++) {
+                this.verItems++;
+                this.modelService.getModelo(this.myData[this.verItems].id).then((res) => {
+                    this.myData[this.verItems] = res
+                }, (err) => {
+                    console.log(err);
+                });
 
-   shared(){
-    window.open('twitter://user?screen_name=xola139', '_system', 'location=no');
-  
-   }
+            }
+        }
+    }
+
+    gotoCard(id) {
+        var top = $('#card_' + id).position().top;
+        $('html, body').animate({
+            scrollTop: (top - 60)
+        }, 'slow');
+    }
+
+    shared() {
+        window.open('twitter://user?screen_name=xola139', '_system', 'location=no');
+
+    }
 
 
 
